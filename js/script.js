6fb75753cce51ab7e75c4c261258b67e769bb52d
kota-shidara <3t.u1.kota@gmail.com>
2018-11-25 01:46:21 +0900

    create modal

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
 $(function() {
 
+    for (let number = 1; number <= 13; number ++) {
+        $('#modal-show_' + number).click(function() {
+            $('#modal-painting-wrapper_' + number).fadeIn();
+            $('#mask').fadeIn();
+            modalResize();
+            function modalResize() {
+                let w = $(window).width();
+                let t = $('#modal-show_' + number).offset().top;
+                let cw = $('#modal-painting-wrapper_' + number).outerWidth();
+                let chl = $('#modal-painting-wrapper_' + number).outerHeight();
+                let chs = $('#modal-show_' + number).outerHeight();
+                $('#modal-painting-wrapper_' + number).css( {
+                    'left' : (w - cw) / 2 ,
+                    'top' : t - (chl - chs)
+                });
+            }
+            $('#mask').hover(function() {
+                $('#mask').css('cursor', 'zoom-out');
+            });
+            $('#mask').click(function() {
+                $('#mask').fadeOut();
+                $('#modal-painting-wrapper_' + number).fadeOut();
+            });
+        });
+    }
 
 });
