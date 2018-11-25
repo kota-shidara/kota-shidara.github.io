e0cbdc18d329b90e7949c15a0a89f5ff0e56a354
kota-shidara <3t.u1.kota@gmail.com>
2018-11-25 10:52:31 +0900

    change class name modal-show_ => painting-modal-show_

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function() {
     for (let number = 1; number <= 13; number ++) {
-        $('#modal-show_' + number).click(function() {
+        $('#painting-modal-show_' + number).click(function() {
             $('#modal-painting-wrapper_' + number).fadeIn();
             $('#mask').fadeIn();
             modalResize();
@@ -8,13 +8,13 @@ $(function() {
                 $('#mask').fadeOut();
                 $('#modal-painting-wrapper_' + number).fadeOut();
             });
-            
+
             function modalResize() {
                 let w = $(window).width();
-                let t = $('#modal-show_' + number).offset().top;
+                let t = $('#painting-modal-show_' + number).offset().top;
                 let cw = $('#modal-painting-wrapper_' + number).outerWidth();
                 let chl = $('#modal-painting-wrapper_' + number).outerHeight();
-                let chs = $('#modal-show_' + number).outerHeight();
+                let chs = $('#painting-modal-show_' + number).outerHeight();
                 $('#modal-painting-wrapper_' + number).css( {
                     'left' : (w - cw) / 2 ,
                     'top' : t - (chl - chs)
