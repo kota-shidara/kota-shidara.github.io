8e009622da1348437dff1cb0833572934a0ac15b
kota-shidara <3t.u1.kota@gmail.com>
2018-11-27 17:02:40 +0900

    change class name "modal-painting-wrapper" => "painting-modal-wrapper"

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 $(function() {
     for (let number = 1; number <= 13; number ++) {
         $('#painting-modal-show_' + number).click(function() {
-            $('#modal-painting-wrapper_' + number).fadeIn();
+            $('#painting-modal-wrapper_' + number).fadeIn();
             $('#mask').fadeIn();
             modalResize();
             $('#mask').click(function() {
                 $('#mask').fadeOut();
-                $('#modal-painting-wrapper_' + number).fadeOut();
+                $('#painting-modal-wrapper_' + number).fadeOut();
             });
 
             function modalResize() {
                 let w = $(window).width();
                 let t = $('#painting-modal-show_' + number).offset().top;
-                let cw = $('#modal-painting-wrapper_' + number).outerWidth();
-                let chl = $('#modal-painting-wrapper_' + number).outerHeight();
+                let cw = $('#painting-modal-wrapper_' + number).outerWidth();
+                let chl = $('#painting-modal-wrapper_' + number).outerHeight();
                 let chs = $('#painting-modal-show_' + number).outerHeight();
-                $('#modal-painting-wrapper_' + number).css( {
+                $('#painting-modal-wrapper_' + number).css( {
                     'left' : (w - cw) / 2 ,
                     'top' : t - (chl - chs)
                 });
