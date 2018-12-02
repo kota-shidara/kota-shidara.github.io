2fbd2425f372f6d974e8f25ac3a8c8c046040002
kota-shidara <3t.u1.kota@gmail.com>
2018-12-02 19:49:12 +0900

    change hiden => hidden

--- js/accordion.js
+++ js/accordion.js
@@ -2,7 +2,7 @@ $(function() {
     $('[data-accordion-btn]').click(function() {
         let target = $(this).data('accordion-btn');
         $(`[data-accordion-target=${target}]`).slideToggle();
-        $(`[data-accordion-btn=${target}]`).toggleClass('is-hiden');
+        $(`[data-accordion-btn=${target}]`).toggleClass('is-hidden');
         console.log(target);
     });
 });
